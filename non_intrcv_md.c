d24c575e49c07f6e5b6057660afe30936170b5ba
JacksonMusee <jacksonmutisya94@gmail.com>
2023-12-02 07:34:06 -0800

    Push

--- non_intrcv_md.c
+++ non_intrcv_md.c
@@ -10,43 +10,20 @@
 void non_intrcv(char **argv, char **env)
 {
 	char *ln_buf = NULL;
-	char *cl_ln_buf = NULL;
-	int i = 0;
-	int j = 0;
+	size_t len = 0;
 
-	ln_buf = read_line();
-	cl_ln_buf = malloc(sizeof(ln_buf));
-
-	while (ln_buf[i] != '\0')
+	while (getline(&ln_buf, &len, stdin) != -1)
 	{
-		if (ln_buf[i] != ' ' && ln_buf[i] != '\t' && ln_buf[i] != '\n')
+		ln_buf[strcspn(ln_buf, "\n")] = '\0';
+
+		argv[0] = strtok(ln_buf, " \t");
+
+		while (argv[0])
 		{
-			cl_ln_buf[j] = ln_buf[i];
-			i++;
-			j++;
-		}
-		else
-		{
-			if (cl_ln_buf[j - 1] != ' ' && cl_ln_buf[j - 1] != '\t' && cl_ln_buf[j - 1] != '\n')
-			{
-				cl_ln_buf[j] = ' ';
-				i++;
-				j++;
-			}
-			else
-			{
-				i++;
-			}
+			execute(argv[0], argv, env);
+			argv[0] = strtok(NULL, " \t");
 		}
 
-	}
-	cl_ln_buf[j+1] = '\0';
-
-	argv[0] = strtok(cl_ln_buf, " ");
-
-	while (argv[0])
-	{
-		execute(argv[0], argv, env);
-		argv[0] = strtok(NULL, " ");
+		free(ln_buf);
 	}
 }
